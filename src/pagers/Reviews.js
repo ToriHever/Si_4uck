e46458092875bc8dc7ba7e90ac840b26612d8bee
Vi <vika.miroshnikova2016@gmail.com>
2023-04-11 21:17:20 +0300

    Переменые с элементами из DOM "Отзывы"

--- src/pagers/Reviews.js
+++ src/pagers/Reviews.js
@@ -1,3 +1,24 @@
 import * as $ from 'jquery'
 import '@styles/burger_menu.scss'
 import '@media/header_footer.scss'
+
+import {isValid} from './utils.js'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitbtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    if (isValid(input.value)) {
+        const question =  {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        // Async тут ассинхроный запрос на сервер
+        }
+}
