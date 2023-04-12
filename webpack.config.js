34d01d4ac972b9f9ba7fe57c8426d4ca072c1b0f
Vi <vika.miroshnikova2016@gmail.com>
2023-04-12 23:41:05 +0300

    все страницы билдяться в pagers

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist/pagers')
     },
     resolve: {
         extensions: ['.js', '.json', '.png'],
